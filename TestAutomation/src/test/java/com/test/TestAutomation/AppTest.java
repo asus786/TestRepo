9a636dbf590a69ca09ee400f183b2db60011c158
Sajjad.Rasul <786dell@gmail.com>
2019-04-10 22:58:49 -0400

    first commit

---
+++ TestAutomation/src/test/java/com/test/TestAutomation/AppTest.java
@@ -0,0 +1,15 @@
+package com.test.TestAutomation;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+	@Test
+	public void launchPage() {
+		System.out.println(".......................Test started...");
+		
+	}
+}
